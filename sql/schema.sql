26a965acb3cee81ce720a8122765b2d0245d819e
Richard <richard.yang.tong@gmail.com>
2021-02-20 18:10:12 -0500

    Added table employee.

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,3 +3,12 @@ DROP DATABASE IF EXIST employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY(id)
+);

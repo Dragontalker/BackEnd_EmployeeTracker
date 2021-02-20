eb8de897856b245c20d3d6dedb0863046ae7330b
Richard <richard.yang.tong@gmail.com>
2021-02-20 18:14:40 -0500

    Added table role.

--- sql/schema.sql
+++ sql/schema.sql
@@ -8,7 +8,15 @@ CREATE TABLE employee (
     id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(10, 2),
+    department_id INT NOT NULL,
     PRIMARY KEY(id)
 );

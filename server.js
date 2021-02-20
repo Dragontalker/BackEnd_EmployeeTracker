cb45eed57ea75bbeddf35075257578dede9619a2
Richard <richard.yang.tong@gmail.com>
2021-02-20 18:04:45 -0500

    Started a express server.

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 // dependencies
 const express = require('express');
 
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server started at port ${PORT}...`);
+});

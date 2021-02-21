f33fdaa4e2cef284b1b909bd0beba36e27fe94d2
Richard <richard.yang.tong@gmail.com>
2021-02-21 10:18:38 -0500

    Renamed the connection function to be more descriptive.

--- src/connection.js
+++ src/connection.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const connectDb = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',

aa7019211c48adcc2e5f2b1e180dcd199cca8bc2
Richard <richard.yang.tong@gmail.com>
2021-02-21 10:17:53 -0500

    Added a database connection.

--- src/connection.js
+++ src/connection.js
@@ -1,2 +1,9 @@
 const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'sql23248265YT',
+    database: 'employee_db'
+});

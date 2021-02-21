d6d7c69e63d48956003c0a206782a5c30546840d
Richard <richard.yang.tong@gmail.com>
2021-02-21 11:43:38 -0500

    Added a new employee row Tom Allen.

--- sql/seed_employees.sql
+++ sql/seed_employees.sql
@@ -15,3 +15,6 @@ VALUES ('Malia', 'Brown', 5);
 
 INSERT INTO employees (first_name, last_name, role_id)
 VALUES ('Sarah', 'Lourd', 6);
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES ('Tom', 'Allen', 7);

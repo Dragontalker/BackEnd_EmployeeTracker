016fa137cbc76ada9abe49198f58075de00356f3
Richard <richard.yang.tong@gmail.com>
2021-02-21 11:57:09 -0500

    Fixed the identation.

--- src/showEmployees.js
+++ src/showEmployees.js
@@ -4,9 +4,9 @@ const connectDb = require('./connection');
 const showEmployees = () => {
     let sql = 'SELECT * FROM employees';
     connectDb.query(sql, (error, result) => {
-            if (error) throw error;
-            console.table(result);
-        });
+        if (error) throw error;
+        console.table(result);
+    });
 };
 
 module.exports = showEmployees;

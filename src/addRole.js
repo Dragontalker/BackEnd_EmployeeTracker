d1e2f118b4f3993f1b80b1e2142bbbfca2d1ff4d
Richard <richard.yang.tong@gmail.com>
2021-02-21 14:24:30 -0500

    Updated the callback function in module addRole.

--- src/addRole.js
+++ src/addRole.js
@@ -7,7 +7,7 @@ const addRoles = (title, salary, departmentId) => {
         department_id: `${departmentId}`
     };
     let sql = `INSERT INTO roles SET ?`;
-    connectDb.query(sql, post, (error, result) => {
+    connectDb.query(sql, post, (error, rows, fields) => {
         if (error) throw error;
         console.log(`New role: ${title} has been added to database.`);
         console.log(`The department ID is ${departmentId}.`);

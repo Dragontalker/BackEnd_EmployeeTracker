361f654baeefa839f6b0c66be990c6a727962b4b
Richard <richard.yang.tong@gmail.com>
2021-02-21 11:47:01 -0500

    Connected the showEmployees module to main app and tested it.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const showDepartments = require('./src/showDepartments')
 const showRoles = require('./src/showRoles');
+const showEmployees = require('./src/showEmployees');
 
 // showDepartments();
-showRoles();
+// showRoles();
+showEmployees();
 
 

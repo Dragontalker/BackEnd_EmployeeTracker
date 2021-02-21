652fecb627e78a3611a910cb0755aa4706de5c96
Richard <richard.yang.tong@gmail.com>
2021-02-21 14:22:14 -0500

    Updated the callback function in module addDepartment.

--- src/addDepartment.js
+++ src/addDepartment.js
@@ -3,7 +3,7 @@ const connectDb = require('./connection');
 const addDepartment = (department) => {
     let post = { name: `${department}`};
     let sql = `INSERT INTO departments SET ?`;
-    connectDb.query(sql, post, (error, result) => {
+    connectDb.query(sql, post, (error, rows, fields) => {
         if (error) throw error;
         console.log(`New department: ${department} has been added to database.`);
     });

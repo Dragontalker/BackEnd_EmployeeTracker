c306102cea99247592bff12437ff895af93e3735
Richard <richard.yang.tong@gmail.com>
2021-02-21 10:37:49 -0500

    Removed the async await from display function and tested it.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const cTable = require('console.table');
 const connectDb = require('./src/connection');
 
-const showDepartment = async () => {
-    await connectDb.query(
+const showDepartment = () => {
+    connectDb.query(
         'SELECT * FROM department', 
         function(error, result) {
             if (error) throw error;

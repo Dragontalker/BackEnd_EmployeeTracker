1c6ee413d866ed10a60b530f97d32199088f6723
Richard <richard.yang.tong@gmail.com>
2021-02-21 11:41:52 -0500

    Added a new employee row for Kevin Tupik.

--- sql/seed_employees.sql
+++ sql/seed_employees.sql
@@ -6,3 +6,6 @@ VALUES ('Mike', 'Chan', 2);
 
 INSERT INTO employees (first_name, last_name, role_id)
 VALUES ('Ashley', 'Rodriguez', 3);
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES ('Kevin', 'Tupik', 4);

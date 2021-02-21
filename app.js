3ce9d9b353036abd17841018545479120ce0c7f0
Richard <richard.yang.tong@gmail.com>
2021-02-21 10:41:35 -0500

    Created a seprate module for showDepartment to maintain good modualarity.

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-const cTable = require('console.table');
-const connectDb = require('./src/connection');
-
-const showDepartment = () => {
-    connectDb.query(
-        'SELECT * FROM department', 
-        function(error, result) {
-            if (error) throw error;
-            console.table(result);
-        }
-    );
-};
+const showDepartment = require('./src/showDepartment')
 
 showDepartment();
 

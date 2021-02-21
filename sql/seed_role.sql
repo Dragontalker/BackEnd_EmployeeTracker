c93358af9f5f5b6801c471ba5de3f33d5e7f283c
Richard <richard.yang.tong@gmail.com>
2021-02-21 11:12:55 -0500

    Added a new role for lead engineer.

--- sql/seed_role.sql
+++ sql/seed_role.sql
@@ -5,6 +5,9 @@ INSERT INTO role (title, salary, department_id)
 VALUE ('Salesperson', 80000, 1);
 
 INSERT INTO role (title, salary, department_id)
+VALUE ('Lead Engineer', 150000, 2);
+
+INSERT INTO role (title, salary, department_id)
 VALUE ('Lawyer', 190000, 4);
 
 INSERT INTO role (title, salary, department_id)

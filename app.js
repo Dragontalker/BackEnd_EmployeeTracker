4bdc94b950763bdc21631c4f44019b31096cc059
Richard <richard.yang.tong@gmail.com>
2021-02-21 13:49:04 -0500

    Connected addRole module to main app and tested it.

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const showDepartments = require('./src/showDepartments')
 const showRoles = require('./src/showRoles');
 const showEmployees = require('./src/showEmployees');
 const addDepartment = require('./src/addDepartment');
+const addRole = require('./src/addRole')
 
 const testApp = async () => {
-    await addDepartment('Operation');
-    showDepartments();
+    await addRole('Junior Software Engineer', 105000, 2);
+    showRoles();
 };
 
 testApp();
--- src/addRole.js
+++ src/addRole.js
@@ -9,7 +9,7 @@ const addRole = (title, salary, departmentId) => {
     let sql = `INSERT INTO roles SET ?`;
     connectDb.query(sql, post, (error, result) => {
         if (error) throw error;
-        console.log(`New role: ${department} has been added to database.`);
+        console.log(`New role: ${title} has been added to database.`);
         console.log(`The department ID is ${departmentId}.`);
         console.log(`With salary: ${salary}.`);
     });

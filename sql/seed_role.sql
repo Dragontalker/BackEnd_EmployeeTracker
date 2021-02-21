7d3986126d938114d53ff36af7ff1c89975011bf
Richard <richard.yang.tong@gmail.com>
2021-02-21 11:12:05 -0500

    Added a row for salesperson.

--- sql/seed_role.sql
+++ sql/seed_role.sql
@@ -2,6 +2,9 @@ INSERT INTO role (title, salary, department_id)
 VALUE ('Sales Lead', 100000, 1);
 
 INSERT INTO role (title, salary, department_id)
+VALUE ('Salesperson', 80000, 1);
+
+INSERT INTO role (title, salary, department_id)
 VALUE ('Lawyer', 190000, 4);
 
 INSERT INTO role (title, salary, department_id)

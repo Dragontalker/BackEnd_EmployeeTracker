eb23575df466d0834befead1ff8446c63ac33f2c
Richard <richard.yang.tong@gmail.com>
2021-02-21 10:36:02 -0500

    Added a feature to display all department names in table.

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const cTable = require('console.table');
 const connectDb = require('./src/connection');
 
+const showDepartment = async () => {
+    await connectDb.query(
+        'SELECT * FROM department', 
+        function(error, result) {
+            if (error) throw error;
+            console.table(result);
+        });
+};
+
+showDepartment();
+
 

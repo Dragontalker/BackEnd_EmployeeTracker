9dcef9118026df0dd649f1e82ac9156a92b2237c
Richard <richard.yang.tong@gmail.com>
2021-02-21 11:33:49 -0500

    Added a new employee row for Ashley Rodriguez.

--- sql/seed_employees.sql
+++ sql/seed_employees.sql
@@ -3,3 +3,6 @@ VALUES ('John', 'Doe', 1);
 
 INSERT INTO employees (first_name, last_name, role_id)
 VALUES ('Mike', 'Chan', 2);
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES ('Ashley', 'Rodriguez', 3);

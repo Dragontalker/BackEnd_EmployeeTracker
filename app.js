55a4f33a3042162f643fe280d46f785e6db79f39
Richard <richard.yang.tong@gmail.com>
2021-02-21 11:59:12 -0500

    Improved showRoles module with ES6 practice.

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const showDepartments = require('./src/showDepartments')
 const showRoles = require('./src/showRoles');
 const showEmployees = require('./src/showEmployees');
 
-showDepartments();
-// showRoles();
+// showDepartments();
+showRoles();
 // showEmployees();
 
 
--- src/showRoles.js
+++ src/showRoles.js
@@ -2,13 +2,11 @@ const cTable = require('console.table');
 const connectDb = require('./connection');
 
 const showRoles = () => {
-    connectDb.query(
-        'SELECT * FROM roles', 
-        function(error, result) {
-            if (error) throw error;
-            console.table(result);
-        }
-    );
+    let sql = 'SELECT * FROM roles';
+    connectDb.query(sql, (error, result) => {
+        if (error) throw error;
+        console.table(result);
+    });
 };
 
 module.exports = showRoles;

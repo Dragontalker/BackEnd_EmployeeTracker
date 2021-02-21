39cd226a3c07ec2521148caddbc9a859e565770e
Richard <richard.yang.tong@gmail.com>
2021-02-21 13:34:46 -0500

    Added a module to add new department.

---
+++ src/addDepartment.js
@@ -0,0 +1,13 @@
+const cTable = require('console.table');
+const connectDb = require('./connection');
+
+const addDepartment = (department) => {
+    let post = { name: `${department}`};
+    let sql = `INSERT INTO departments SET ?`;
+    connectDb.query(sql, post, (error, result) => {
+        if (error) throw error;
+        console.table(result);
+    });
+};
+
+module.exports = addDepartment;

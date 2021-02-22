cda732825c7ca7329409095069014294e30bdf96
Richard <richard.yang.tong@gmail.com>
2021-02-22 14:18:34 -0500

    Tested module getRoleNames.

--- src/get/getDepartmentNames.js
+++ src/get/getDepartmentNames.js
@@ -3,7 +3,7 @@ const connectDb = require('../connection');
 const db = connectDb();
 
 const getDepartmentNames = async () => {
-    let sql = 'SELECT DISTINCT name FROM departments';
+    let sql = 'SELECT name FROM departments';
     let result = await db.query(sql);
     await db.close();
     return result.map(obj => obj.name);
--- src/get/getRoleNames.js
+++ src/get/getRoleNames.js
@@ -3,10 +3,18 @@ const connectDb = require('../connection');
 const db = connectDb();
 
 const getRoleNames = async () => {
-    let sql = 'SELECT DISTINCT name FROM roles';
+    let sql = 'SELECT title FROM roles';
     let result = await db.query(sql);
     await db.close();
-    return result.map(obj => obj.name);
+    return result.map(obj => obj.title);
 };
+
+// Test
+const test = async () => {
+    let results = await getRoleNames();
+    console.log(results);
+}
+
+test();
 
 module.exports = getRoleNames;

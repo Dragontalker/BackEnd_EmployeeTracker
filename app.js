47b8c55f9a0c4bf33f138df6a57005b8c3e18bfe
Richard <richard.yang.tong@gmail.com>
2021-02-22 10:42:21 -0500

    Succesfully printed out full name array in console.

--- app.js
+++ app.js
@@ -5,16 +5,12 @@ const addDepartment = require('./src/add/addDepartment');
 const addRole = require('./src/add/addRole');
 const addEmployee = require('./src/add/addEmployee');
 const updateRoleById = require('./src/updateRoleById');
+const getNameArrays = require('./src/get/getNameArrays');
+
 const connectDb = require('./src/connection');
 
 db = connectDb();
 
-const testApp = async () => {
-    let result = await db.getNameArrays();
-    await db.close();
-    console.log(result);
-}
-
-testApp();
+getNameArrays();
 
 
--- src/get/getNameArrays.js
+++ src/get/getNameArrays.js
@@ -1,12 +1,15 @@
+const cTable = require('console.table');
 const connectDb = require('../connection');
 
 const db = connectDb();
 
-const getNameArrays = async() => {
-    let sql = `SELECT first_name, last_name FROM employees'`; 
-    let result = await db.query(sql);
-    console.log(result);
+const getNameArrays = async () => {
+    let sql = 'SELECT first_name, last_name FROM employees';
+    let result = await db.query(sql)
+    let fullName = result.map(obj => `${obj.first_name} ${obj.last_name}`);
+    console.log(fullName);
     await db.close();
 };
+
 
 module.exports = getNameArrays;

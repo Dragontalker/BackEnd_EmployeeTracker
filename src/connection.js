00c892e0e3b3cd313b795eda7541146cddb339e6
Richard <richard.yang.tong@gmail.com>
2021-02-22 10:07:15 -0500

    Rewrote getRoleId into a method for database class.

--- src/connection.js
+++ src/connection.js
@@ -28,6 +28,19 @@ class Database {
             } );
         } );
     }
+
+    getRoleId(roleName) {
+        let sql = `SELECT id FROM roles WHERE title = '${roleName}'`; 
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, (error, rows) => {
+                if (rows === undefined) {
+                    reject(new Error('Error row is undefined'));
+                } else {
+                    resolve(rows[0].id);
+                };
+            });
+        });
+    }
 };
 
 const connectDb = () => {

c7f1d1828183af2e684b34924d696d922f3efc51
Richard <richard.yang.tong@gmail.com>
2021-02-22 10:32:18 -0500

    Rewrote getNameArrays into a module.

--- src/get/getNameArrays.js
+++ src/get/getNameArrays.js
@@ -1,15 +1,12 @@
+const connectDb = require('../connection');
 
+const db = connectDb();
 
-getNameArrays() {
+const getNameArrays = async() => {
     let sql = `SELECT first_name, last_name FROM employees'`; 
-    return new Promise((resolve, reject) => {
-        this.connection.query(sql, (error, rows) => {
-            if (rows === undefined) {
-                reject(new Error('Error row is undefined'));
-            } else {
-                resolve(rows);
-            };
-        });
-    });
-}
+    let result = await db.query(sql);
+    console.log(result);
+    await db.close();
 };
+
+module.exports = getNameArrays;

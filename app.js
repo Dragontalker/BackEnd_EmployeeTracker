8d2e1bdc5f1eb4fb24e94a772999a3d571f65aa7
Richard <richard.yang.tong@gmail.com>
2021-02-22 10:12:56 -0500

    Tested getRoleId method in main app, feature is working properly.

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ const addRole = require('./src/add/addRole');
 const addEmployee = require('./src/add/addEmployee');
 const getRoleId = require('./src//get/getRoleId');
 const updateRoleById = require('./src/updateRoleById');
+const connectDb = require('./src/connection');
 
+db = connectDb();
 
-viewEmployees();
+const testApp = async () => {
+    let result = await db.getRoleId('Lead Engineer');
+    await db.close();
+    console.log(`The result is ${result}!`);
+}
 
+testApp();
 
 

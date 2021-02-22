608b9a13dbd01393c1ff0398c2577a33daf22c41
Richard <richard.yang.tong@gmail.com>
2021-02-22 11:15:18 -0500

    Small indentation change.

--- src/get/getEmployeeId.js
+++ src/get/getEmployeeId.js
@@ -3,7 +3,7 @@ const connectDb = require('../connection');
 const db = connectDb();
 
 const getEmployeeId = async (employeeName) => {
-    let [ firstName, lastName] = employeeName.split(' ');
+    let [ firstName, lastName ] = employeeName.split(' ');
     let sql = `SELECT id FROM employees WHERE first_name = '${firstName}' AND last_name = '${lastName}'`; 
     let result = await db.query(sql);
     await db.close();

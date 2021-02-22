988e4b02f299ed367fea0299cd6c381b0799eba0
Richard <richard.yang.tong@gmail.com>
2021-02-22 13:29:02 -0500

    Created a new inquirer module for adding new role.

---
+++ src/inquirer/inqAddRole.js
@@ -0,0 +1,24 @@
+const inquirer = require("inquirer");
+
+const inqAddRole = (array) => {
+    return inquirer.prompt(
+        {
+            name: "title",
+            type: "input",
+            message: "What is the title of the new role?"
+        },
+        {
+            name: "salary",
+            type: "number",
+            message: "What is the salary of the new role?"
+        },
+        {
+            name: "department",
+            type: "list",
+            message: "Which department does this new role belong to?",
+            choices: array
+        }
+    )
+};
+
+module.exports = inqAddRole;

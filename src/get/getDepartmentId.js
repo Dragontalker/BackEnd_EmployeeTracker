a608e967ce536a1548e6567bc42e04962548f075
Richard <richard.yang.tong@gmail.com>
2021-02-22 13:50:46 -0500

    Created a new module for getting department id given department name.

---
+++ src/get/getDepartmentId.js
@@ -0,0 +1,12 @@
+const connectDb = require('../connection');
+
+const db = connectDb();
+
+const getDepartmentId = async (departmentName) => {
+    let sql = `SELECT id FROM departments WHERE name = '${departmentName}'`; 
+    let result = await db.query(sql);
+    await db.close();
+    return result[0].id;
+};
+
+module.exports = getDepartmentId;
